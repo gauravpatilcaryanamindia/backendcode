e84f191dbf32c6f8d34c9142a8cab16d813806e1
geetanjali3577 <khaladkargeet07@gmail.com>
2026-01-30 11:27:06 +0530

    removeRes

---
+++ master-01/master-01/src/main/java/com/spring/jwt/cart/dto/ResponseDto.java
@@ -0,0 +1,15 @@
+package com.spring.jwt.cart.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDto {
+
+    private String status;
+    private String message;
+    private Object data;
+}
